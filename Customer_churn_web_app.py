36d0b55f5df4d07eda5151f17d9d8cd0511de57e
Oyelayo Seye <seyeoyelayo@gmail.com>
2023-11-20 00:57:51 +0100

    Update Customer_churn_web_app.py

--- Customer_churn_web_app.py
+++ Customer_churn_web_app.py
@@ -1,75 +1,52 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Nov 13 21:49:29 2023
-
-@author: Administrator
-"""
-
 import numpy as np
 import pandas as pd
 import pickle
 import streamlit as st
+import requests
 
-#loading the model
-path = "https://raw.githubusercontent.com/Exwhybaba/Customer_Churn/main/trained2_model.sav"
+# Loading the model
+url = "https://raw.githubusercontent.com/Exwhybaba/Customer_Churn/main/trained2_model.sav"
+response = requests.get(url)
 
+if response.status_code == 200:
+    loaded_model = pickle.loads(response.content)
+else:
+    st.error("Failed to retrieve the model file. Status code: {}".format(response.status_code))
+    st.stop()
 
-loaded_model = pickle.load(open(path, mode= 'rb'))
-
-#prediction function
+# Prediction function
 def churn_prediction(Total_Revolving_Bal, Total_Trans_Amt, Total_Trans_Ct, NB_Classifier_2):
     data = {
-        'Total_Revolving_Bal' : [Total_Revolving_Bal], 
-        'Total_Trans_Amt' : [Total_Trans_Amt], 
-        'Total_Trans_Ct' : [Total_Trans_Ct], 
-        'NB_Classifier_2' : [NB_Classifier_2],
+        'Total_Revolving_Bal': [Total_Revolving_Bal],
+        'Total_Trans_Amt': [Total_Trans_Amt],
+        'Total_Trans_Ct': [Total_Trans_Ct],
+        'NB_Classifier_2': [NB_Classifier_2],
     }
-    #convert the data to pandas
     df = pd.DataFrame(data)
-
-    #convert data numpy array
     df2array = np.asarray(df)
-    #reshape the array
     reshape_array = df2array.reshape(1, -1)
 
-    
     prediction = loaded_model.predict(reshape_array)
 
     if prediction[0] == 1:
         return 'The customer is on the verge of churning.'
     else:
-        return'The customer is not on the verge of churning'
-    
-    
-    
-def main(debug = True):
-    #giving a title
-    st.title('Customer Churn Prediction Web App')
-    st.image(r"C:\Users\Administrator\Documents\AIsat\Group_Project\Customer-Churn.png", width=200)
+        return 'The customer is not on the verge of churning'
 
-    
-    #getting the input data from the user
+def main(debug=True):
+    st.title('Customer Churn Prediction Web App')
+    st.image("https://raw.githubusercontent.com/Exwhybaba/Customer_Churn/main/Customer-Churn.png", width=200)
+
     Total_Revolving_Bal = st.number_input('Kindly input the total revolving balance')
     Total_Trans_Amt = st.number_input('Kindly input total transaction amount')
     Total_Trans_Ct1 = st.number_input('Kindly input total transaction Ct1')
     NB_Classifier_2 = st.number_input('Kindly input NB_Classifier_2')
 
-    #code for prediction
-    
     attrition = ''
-    
-    #creating a button for prediction
+
     if st.button('Customer churn result'):
-        attrition = churn_prediction(Total_Revolving_Bal, 
-                                     Total_Trans_Amt, Total_Trans_Ct1, NB_Classifier_2)
+        attrition = churn_prediction(Total_Revolving_Bal, Total_Trans_Amt, Total_Trans_Ct1, NB_Classifier_2)
     st.success(attrition)
-    
+
 if __name__ == '__main__':
     main()
-
-    
-    
-    
-    
-    
-    
