a82405d0eca70344001b88f8578871888c2f0059
Oyelayo Seye <seyeoyelayo@gmail.com>
2023-11-20 01:23:21 +0100

    Update Customer_churn_web_app.py

--- Customer_churn_web_app.py
+++ Customer_churn_web_app.py
@@ -35,10 +35,7 @@ def churn_prediction(Total_Revolving_Bal, Total_Trans_Amt, Total_Trans_Ct, NB_Cl
 
 def main(debug=True):
     st.title('Customer Churn Prediction Web App')
-    import streamlit as st
-    st.image("https://raw.githubusercontent.com/Exwhybaba/Customer_Churn/main/Customer-Churn.png", use_container_width=True)
-
-
+    st.image("https://raw.githubusercontent.com/Exwhybaba/Customer_Churn/main/Customer-Churn.png")
     Total_Revolving_Bal = st.number_input('Kindly input the total revolving balance')
     Total_Trans_Amt = st.number_input('Kindly input total transaction amount')
     Total_Trans_Ct1 = st.number_input('Kindly input total transaction Ct1')
