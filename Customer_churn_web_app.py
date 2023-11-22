6fb540ebc82589fa4a8ffc2bbcb8ad84bd9b8539
Oyelayo Seye <seyeoyelayo@gmail.com>
2023-11-22 15:55:26 +0100

    Update Customer_churn_web_app.py

--- Customer_churn_web_app.py
+++ Customer_churn_web_app.py
@@ -490,7 +490,7 @@ def main():
     )
 
     # Single column layout
-    col1 = st.column(1)
+    col1, col2 = st.column(1)
 
     # Animated button for prediction with a success icon
     if col1.button('Predict Customer Churn', key='prediction_button', help="Click to predict customer churn"):
