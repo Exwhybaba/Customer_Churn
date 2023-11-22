a0f79630baa220b93c5a5ffae58aca831e5bab5f
Oyelayo Seye <seyeoyelayo@gmail.com>
2023-11-22 16:41:05 +0100

    Update Customer_churn_web_app.py

--- Customer_churn_web_app.py
+++ Customer_churn_web_app.py
@@ -427,10 +427,10 @@ def churn_prediction(Total_Relationship_Count, Total_Revolving_Bal, Total_Amt_Ch
         normalizer_reshape = normalizer.transform(scaler_reshape)
         return normalizer_reshape
 
-    # Transform data
+    #transform data
     transformed_data = transformation(reshape_array)
 
-    # Make prediction
+    # make prediction
     prediction = model.predict(transformed_data)
 
     if prediction[0] == 1:
@@ -439,6 +439,9 @@ def churn_prediction(Total_Relationship_Count, Total_Revolving_Bal, Total_Amt_Ch
         print('The customer is not on the verge of churning')
 
     return df
+
+
+
 
 # Main function
 def main():
@@ -483,54 +486,112 @@ def main():
     st.sidebar.markdown('- **Total Transaction Count**: Enter the total transaction count.')
     st.sidebar.markdown('- **Total Count Change Q4-Q1**: Enter the total count change from Q4 to Q1.')
 
+
     # Main content layout with rounded corners
-    st.markdown(
-        '<style>div.Widget.stButton button{border-radius: 10px;}</style>',
-        unsafe_allow_html=True
-    )
+st.markdown(
+    '<style>div.Widget.stButton button{border-radius: 10px;}</style>',
+    unsafe_allow_html=True
+)
 
-# Single column layout
-col1 = st.container()
+col1, col2 = st.columns(2)
 
-# Animated button for prediction with a success icon
-if col1.button('Predict Customer Churn', key='prediction_button', help="Click to predict customer churn"):
-    with col1.spinner('Predicting...'):
-        # Prediction logic
-        attrition = churn_prediction(Gender, Total_Revolving_Bal, Total_Trans_Amt, Total_Trans_Ct,
-                                     Total_Relationship_Count, Months_Inactive_12_mon)
+# First column
+with col1:
+    Total_Relationship_Count_min = 1
+    Total_Relationship_Count_max = 6
+    Total_Relationship_Count = st.number_input('Total Relationship Count',
+                                               min_value=Total_Relationship_Count_min,
+                                               max_value=Total_Relationship_Count_max,
+                                               value=Total_Relationship_Count_min)
 
-    # Display prediction result with custom styling and icons
-    result_placeholder = col1.empty()
-    if attrition[0] == '1':
-        result_placeholder.error('❗ The customer is on the verge of churning. 🚨')
-    else:
-        result_placeholder.success('🎉 The customer is not on the verge of churning. 🌟')
+    Total_Revolving_Bal_min = 0
+    Total_Revolving_Bal_max = 2517
+    Total_Revolving_Bal = st.number_input('Total Revolving Balance',
+                                          min_value=Total_Revolving_Bal_min,
+                                          max_value=Total_Revolving_Bal_max,
+                                          value=Total_Revolving_Bal_min, 
+                                         )
 
-# Option to upload a file with a file icon
-uploaded_file = col1.file_uploader("Upload a CSV file with customer data", type=["csv"])
-if uploaded_file is not None:
-    # Read the uploaded file
-    uploaded_df = pd.read_csv(uploaded_file)
+    Total_Amt_Chng_Q4_Q1_min = 0.275
+    Total_Amt_Chng_Q4_Q1_max = 1.212
+    Total_Amt_Chng_Q4_Q1 = st.slider('Total Amount Change Q4-Q1',
+                                           min_value=Total_Amt_Chng_Q4_Q1_min,
+                                           max_value=Total_Amt_Chng_Q4_Q1_max,
+                                           value=Total_Amt_Chng_Q4_Q1_min,
+                                       step=0.001)
 
-    # Make predictions for the uploaded data
-    uploaded_df2np = np.asarray(uploaded_df)
-    predicted_value = model.predict(uploaded_df2np)
-    uploaded_df['predicted_churn'] = predicted_value.reshape(-1, 1)
+# Second column
+with col2:
+    Total_Trans_Amt_min = 510
+    Total_Trans_Amt_max = 8618
+    Total_Trans_Amt = st.number_input('Total Transaction Amount',
+                                      min_value=Total_Trans_Amt_min,
+                                      max_value=Total_Trans_Amt_max,
+                                      value=Total_Trans_Amt_min)
 
-    # Download the CSV file with a download icon
-    csv_data = uploaded_df.to_csv(index=False)
-    col1.download_button(
-        label="Download Predicted Results",
-        data=io.StringIO(csv_data).read(),
-        file_name="predicted_results.csv",
-        key='download_button',
-        help="Click to download the predicted results"
-    )
+    Total_Trans_Ct_min = 10
+    Total_Trans_Ct_max = 113
+    Total_Trans_Ct = st.number_input('Total Transaction Count',
+                                     min_value=Total_Trans_Ct_min,
+                                     max_value=Total_Trans_Ct_max,
+                                     value=Total_Trans_Ct_min)
 
-# Real-time updates with placeholder and loading spinner
-result_placeholder = col1.empty()
-result_placeholder.text("Waiting for predictions...")
+    Total_Ct_Chng_Q4_Q1_min = 0.206
+    Total_Ct_Chng_Q4_Q1_max = 1.182
+    Total_Ct_Chng_Q4_Q1 = st.number_input('Total Count Change Q4-Q1',
+                                min_value=Total_Ct_Chng_Q4_Q1_min,
+                                max_value=Total_Ct_Chng_Q4_Q1_max,
+                                value=Total_Ct_Chng_Q4_Q1_min)  
+
+
+
+    # Animated button for prediction with a success icon
+    if st.button('Predict Customer Churn', key='prediction_button', help="Click to predict customer churn"):
+        with st.spinner('Predicting...'):
+            # Prediction logic
+            attrition = churn_prediction(Gender, Total_Revolving_Bal, Total_Trans_Amt, Total_Trans_Ct,
+                                         Total_Relationship_Count, Months_Inactive_12_mon)
+
+        # Display prediction result with custom styling and icons
+        result_placeholder = st.empty()
+        if attrition[0] == '1':
+            result_placeholder.error('❗ The customer is on the verge of churning. 🚨')
+        else:
+            result_placeholder.success('🎉 The customer is not on the verge of churning. 🌟')
+
+    # Option to upload a file with a file icon
+    uploaded_file = st.file_uploader("Upload a CSV file with customer data", type=["csv"])
+    if uploaded_file is not None:
+        # Read the uploaded file
+        uploaded_df = pd.read_csv(uploaded_file)
+
+    
+        # Make predictions for the uploaded data
+        uploaded_df2np = np.asarray(uploaded_df)
+        predicted_value = model.predict(uploaded_df2np)
+        uploaded_df['predicted_churn'] = predicted_value.reshape(-1,1)
+
+        # Download the CSV file with a download icon
+        csv_data = uploaded_df.to_csv(index=False)
+        st.download_button(
+            label="Download Predicted Results",
+            data=io.StringIO(csv_data).read(),
+            file_name="predicted_results.csv",
+            key='download_button',
+            help="Click to download the predicted results"
+        )
+
+    # Real-time updates with placeholder and loading spinner
+    result_placeholder = st.empty()
+    result_placeholder.text("Waiting for predictions...")
+
+    # Helper function to set page config only once
+    def set_page_config_once():
+        if not hasattr(st, '_page_config_called'):
+            st.set_page_config(layout="wide")
+            st._page_config_called = True
 
 
 if __name__ == '__main__':
     main()
+
