e61289598d8fa1b4564c473718152ee9f275309d
Oyelayo Seye <seyeoyelayo@gmail.com>
2023-11-22 17:38:07 +0100

    Update Customer_churn_web_app.py

--- Customer_churn_web_app.py
+++ Customer_churn_web_app.py
@@ -34,417 +34,84 @@ from sklearn.metrics import confusion_matrix
 
 path = "https://raw.githubusercontent.com/Exwhybaba/Customer_Churn/main/credit_card_churn.csv"
 
-# In[5]:
-
-
-#with zf.ZipFile(path, 'r') as myfile:
-    #myfile.extractall()
-
-
-# In[6]:
-
-
-#filePaths = r"C:\Users\Administrator\Documents\AIsat\Group_Project\datasets"
-
-
 df1 = pd.read_csv(path)
 
-# renaming the long columns
-df1.rename(columns={'Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_1':'NB_Classifier_1',
-       'Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_2': 'NB_Classifier_2'}, 
+# Renaming columns
+df1.rename(columns={'Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_1': 'NB_Classifier_1',
+                   'Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_2': 'NB_Classifier_2'},
           inplace=True)
 
-
-
+# Binning Age
 bins = [20, 35, 50, 100]
 labels = ['Young Adult', 'Adult', 'Senior']
-df1['Age_Group'] = pd.cut(df1['Customer_Age'], bins= bins, labels= labels)
+df1['Age_Group'] = pd.cut(df1['Customer_Age'], bins=bins, labels=labels)
 
+# Extracting categorical and numerical columns
+categorical = df1.select_dtypes('object')
+numerical = df1.select_dtypes('number')
 
-# In[19]:
+# Handle outliers more efficiently
+def remove_outliers(data, column):
+    q1 = data[column].quantile(0.25)
+    q3 = data[column].quantile(0.75)
+    iqr = q3 - q1
+    lower_bound = q1 - 1.5 * iqr
+    upper_bound = q3 + 1.5 * iqr
+    return data[(data[column] >= lower_bound) & (data[column] <= upper_bound)]
 
+columns_to_remove_outliers = ['Total_Trans_Amt', 'Total_Amt_Chng_Q4_Q1', 'Total_Ct_Chng_Q4_Q1']
+for col in columns_to_remove_outliers:
+    df1 = remove_outliers(df1, col)
 
-#mapping the categorical data
-catgorical = df1.select_dtypes('object')
+# Encoding categorical variables
+ordinal_encoders = {}
+for col in categorical.columns:
+    ordinal_encoders[col] = OrdinalEncoder().fit(df1[[col]])
+    df1[col] = ordinal_encoders[col].transform(df1[[col]])
 
-
-
-
-number = df1.select_dtypes('number')
-
-
-# In[31]:
-
-
-
-# ### Treating for Outliers
-
-# In[51]:
-
-
-outliers = []
-def detect_outliers_iqr(data):
-    data = sorted(data)
-    q1 = np.percentile(data, 25)
-    q3 = np.percentile(data, 75)
-    # print(q1, q3)
-    IQR = q3-q1
-    lwr_bound = q1-(1.5*IQR)
-    upr_bound = q3+(1.5*IQR)
-    # print(lwr_bound, upr_bound)
-    for i in data: 
-        if (i<lwr_bound or i>upr_bound):
-            outliers.append(i)
-    return outliers
-
-
-# In[53]:
-
-
-sample_outliers = detect_outliers_iqr(df1['Total_Trans_Amt'])
-df1.drop(df1[df1['Total_Trans_Amt'].isin(sample_outliers)].index, inplace=True)
-
-
-# In[55]:
-
-
-sample_outliers = detect_outliers_iqr(df1['Total_Amt_Chng_Q4_Q1'])
-df1.drop(df1[df1['Total_Amt_Chng_Q4_Q1'].isin(sample_outliers)].index, inplace=True)
-
-
-# In[56]:
-
-
-
-# In[57]:
-
-
-sample_outliers = detect_outliers_iqr(df1['Total_Ct_Chng_Q4_Q1'])
-df1.drop(df1[df1['Total_Ct_Chng_Q4_Q1'].isin(sample_outliers)].index, inplace=True)
-
-
-# ## Encoding
-
-# In[61]:
-
-
-cate = df1.select_dtypes('object')
-
-
-
-# In[62]:
-
-
-for i in cate.columns:
-    print(i, cate[i].unique())
-
-
-# In[63]:
-
-
-df1['Age_Group'].unique()
-
-
-
-# In[65]:
-
-
-df1['Attrition_Flag'].unique()
-
-
-# In[67]:
-
-
-feature = [f for f in df1.columns if f != 'Attrition_Flag']
-target =  [t for t in df1.columns if t == 'Attrition_Flag']
-
-X = df1[feature]
-y = df1[target]
-
-X_train, X_test, y_train, y_test = train_test_split(X, y,
-test_size= 0.20, random_state=42)
-
-
-# In[68]:
-
-
-y_train['Attrition_Flag'] = y_train['Attrition_Flag'].map(lambda x : 1 if x == 'Attrited Customer' else 0 )
-y_test['Attrition_Flag'] = y_test['Attrition_Flag'].map(lambda x : 1 if x == 'Attrited Customer' else 0 )
-
-
-# In[69]:
-
+# Splitting the dataset
+feature_cols = [col for col in df1.columns if col != 'Attrition_Flag']
+target_col = 'Attrition_Flag'
+X_train, X_test, y_train, y_test = train_test_split(df1[feature_cols], df1[target_col], test_size=0.20, random_state=42)
 
 # Apply SMOTENC to balance the dataset
-categorical_features = [2, 4, 5,6, 7,22]
+categorical_features = [2, 4, 5, 6, 7, 22]
 smotenc = SMOTENC(sampling_strategy='auto', categorical_features=categorical_features, random_state=42)
-X_resampled, y_resampled = smotenc.fit_resample(X_train, y_train)
-X_retst, y_retst = smotenc.fit_resample(X_test, y_test)
+X_train, y_train = smotenc.fit_resample(X_train, y_train)
+X_test, y_test = smotenc.fit_resample(X_test, y_test)
 
+# Feature Engineering - Encoding
+ordinal_cols_to_encode = ['Education_Level', 'Marital_Status', 'Income_Category', 'Card_Category', 'Age_Group']
+for col in ordinal_cols_to_encode:
+    X_train[col] = ordinal_encoders[col].transform(X_train[[col]])
+    X_test[col] = ordinal_encoders[col].transform(X_test[[col]])
 
-# In[70]:
-
-
-X_train = X_resampled
-y_train =  y_resampled
-
-X_test = X_retst
-y_test = y_retst
-
-
-# In[71]:
-
-# ## Encoding
-
-# In[72]:
-
-
-#Education Encoding
-
-# Define the order of categories
-education_order = ['Unknown','Uneducated', 'College', 'Graduate', 'High School', 'Post-Graduate', 'Doctorate']
-
-# Initialize the OrdinalEncoder with the specified order
-edu_ordinal_encoder = OrdinalEncoder(categories=[education_order]).fit(X_train[['Education_Level']])
-
-# Fit and transform on the training data
-X_train['Education_Level'] = edu_ordinal_encoder.transform(X_train[['Education_Level']])
-
-# Transform the test data using the same encoder
-X_test['Education_Level'] = edu_ordinal_encoder.transform(X_test[['Education_Level']])
-
-
-
-# Marital encoding
-# Define the order of categories
-marital_order = ['Unknown','Single', 'Married', 'Divorced']
-
-# Initialize the OrdinalEncoder with the specified order
-mar_ordinal_encoder = OrdinalEncoder(categories=[marital_order]).fit(X_train[['Marital_Status']])
-
-# Fit and transform on the training data
-X_train['Marital_Status'] = mar_ordinal_encoder.transform(X_train[['Marital_Status']])
-
-# Transform the test data using the same encoder
-X_test['Marital_Status'] = mar_ordinal_encoder.transform(X_test[['Marital_Status']])
-
-
-
-#Income encodng
-# Define the order of categories
-income_order = ['Unknown','Less than $40K', '$40K - $60K', '$60K - $80K', 
-       '$80K - $120K', '$120K +']
-
-# Initialize the OrdinalEncoder with the specified order
-inc_ordinal_encoder = OrdinalEncoder(categories=[income_order]).fit(X_train[['Income_Category']])
-
-# Fit and transform on the training data
-X_train['Income_Category'] = inc_ordinal_encoder.transform(X_train[['Income_Category']])
-
-# Transform the test data using the same encoder
-X_test['Income_Category'] = inc_ordinal_encoder.transform(X_test[['Income_Category']])
-
-
-
-
-#Card encoding
-# Define the order of categories
-card_order = ['Blue', 'Silver', 'Gold', 'Platinum']
-
-# Initialize the OrdinalEncoder with the specified order
-card_ordinal_encoder = OrdinalEncoder(categories=[card_order]).fit(X_train[['Card_Category']])
-
-# Fit and transform on the training data
-X_train['Card_Category'] = card_ordinal_encoder.transform(X_train[['Card_Category']])
-
-# Transform the test data using the same encoder
-X_test['Card_Category'] = card_ordinal_encoder.transform(X_test[['Card_Category']])
-
-
-
-
-
-#Age Encoding
-# Define the order of categories
-age_order = ['Adult', 'Senior', 'Young Adult']
-
-# Initialize the OrdinalEncoder with the specified order
-age_ordinal_encoder = OrdinalEncoder(categories=[age_order]).fit(X_train[['Age_Group']])
-
-# Fit and transform on the training data
-X_train['Age_Group'] = age_ordinal_encoder.transform(X_train[['Age_Group']])
-
-# Transform the test data using the same encoder
-X_test['Age_Group'] = age_ordinal_encoder.transform(X_test[['Age_Group']])
-
-
-
-
-#Gender encoding
-# Initialize the Binarizer
-binarizer = LabelBinarizer().fit(X_train['Gender'])
-X_train['Gender']= binarizer.transform(X_train['Gender'])
-
+# Feature Engineering - Binarization
+binarizer = LabelBinarizer()
+X_train['Gender'] = binarizer.fit_transform(X_train['Gender'])
 X_test['Gender'] = binarizer.transform(X_test['Gender'])
 
+# Feature Scaling
+scaler = MinMaxScaler(feature_range=(0, 1)).fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
 
-# In[75]:
+# Feature Normalization
+normalizer = Normalizer().fit(X_train)
+X_train = normalizer.transform(X_train)
+X_test = normalizer.transform(X_test)
 
-
-X_train_df = pd.DataFrame(X_train, columns = feature)
-
-
-
-# In[76]:
-
-
-X_test_df = pd.DataFrame(X_test, columns = feature)
-
-
-# ## Features Selection
-
-# In[77]:
-
-
-X_train_df['Attrition_Flag'] = y_train
-
-
-
-
-# In[79]:
-
-
-columntoDrop = ['Avg_Open_To_Buy', 'NB_Classifier_2', 'NB_Classifier_1']
-X_test_df.drop(columns=columntoDrop, inplace= True)
-X_train_df.drop(columns=columntoDrop, inplace= True)
-
-
-# In[81]:
-
-
-columntoDrop = ['Attrition_Flag']
-X_train_df.drop(columns=columntoDrop, inplace= True)
-
-
-
-RFE_ = ['Total_Relationship_Count', 'Total_Revolving_Bal', 'Total_Amt_Chng_Q4_Q1','Total_Trans_Amt', 'Total_Trans_Ct', 'Total_Ct_Chng_Q4_Q1']
-
-
-
-# In[84]:
-
-# In[85]:
-
-
-X_train = X_train_df[RFE_].values
-X_test = X_test_df[RFE_].values
-
-
-#Rescaling
-scaler = MinMaxScaler(feature_range = (0,1)).fit(X_train)
-R_Xtrain = scaler.transform(X_train)
-R_Xtest = scaler.transform(X_test)
-
-
-# In[89]:
-
-
-R_Xtrain_df = pd.DataFrame(R_Xtrain, columns = X_train_df[RFE_].columns)
-
-
-
-
-# In[91]:
-
-
-#Normalizer
-normalizer = Normalizer().fit(R_Xtrain)
-NR_Xtrain = normalizer.transform(R_Xtrain)
-NR_Xtest = normalizer.transform(R_Xtest)
-
-
-# In[92]:
-
-
-NR_Xtrain_df = pd.DataFrame(NR_Xtrain, columns = X_train_df[RFE_].columns)
-
-
-# In[93]:
-
-
-
-# In[94]:
-
-
-X_train = NR_Xtrain
-X_test = NR_Xtest
-
-
-# In[95]:
-
-
-
-
-
-
-model = model = RandomForestClassifier(n_estimators= 1000)
+# Model Training
+model = RandomForestClassifier(n_estimators=1000)
 model.fit(X_train, y_train)
-y_pred = model.predict(X_test)
 
-
-
-
-# In[103]:
-
-## Loading the model
-#url = #"https://drive.google.com/file/d/1FFz55ZI78 -PwqmESTaTojfYRitImrVxm/view?usp=drive_link"
-#response = requests.get(url)
-
-#if response.status_code == 200:
-    #loaded_model = pickle.loads(response.content)
-#else:
-    #st.error("Failed to retrieve the model file. #Status code: {}".format(response.status_code))
-    #st.stop()
-
-# Function for churn prediction
 # Function for churn prediction
 def churn_prediction(Total_Relationship_Count, Total_Revolving_Bal, Total_Amt_Chng_Q4_Q1,
-                    Total_Trans_Amt, Total_Trans_Ct, Total_Ct_Chng_Q4_Q1):
-    data = {
-        'Total_Relationship_Count': [Total_Relationship_Count],
-        'Total_Revolving_Bal': [Total_Revolving_Bal],
-        'Total_Amt_Chng_Q4_Q1': [Total_Amt_Chng_Q4_Q1],
-        'Total_Trans_Amt': [Total_Trans_Amt],
-        'Total_Trans_Ct': [Total_Trans_Ct],
-        'Total_Ct_Chng_Q4_Q1': [Total_Ct_Chng_Q4_Q1]
-    }
-
-    df = pd.DataFrame(data)
-    df2array = np.asarray(df)
-    reshape_array = df2array.reshape(1, -1)
-
-    def transformation(reshape_array):
-        scaler_reshape = scaler.transform(reshape_array)
-        normalizer_reshape = normalizer.transform(scaler_reshape)
-        return normalizer_reshape
-
-    # Transform data
-    transformed_data = transformation(reshape_array)
-
-    # Make prediction
+                        Total_Trans_Amt, Total_Trans_Ct, Total_Ct_Chng_Q4_Q1):
+    transformed_data = normalizer.transform(scaler.transform(Total_Relationship_Count, Total_Revolving_Bal, Total_Amt_Chng_Q4_Q1,
+                        Total_Trans_Amt, Total_Trans_Ct, Total_Ct_Chng_Q4_Q1))
     prediction = model.predict(transformed_data)
-
-    # Add predicted_churn column to the DataFrame
-    df['predicted_churn'] = prediction
-
-    if prediction[0] == 1:
-        print('The customer is on the verge of churning.')
-    else:
-        print('The customer is not on the verge of churning')
-
-    return df
-
-
+    return prediction
 
 
 
