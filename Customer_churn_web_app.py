c5e0ef40471a67444157c9796ce1b99f357ed74b
Oyelayo Seye <seyeoyelayo@gmail.com>
2023-11-23 13:25:36 +0100

    Update Customer_churn_web_app.py

--- Customer_churn_web_app.py
+++ Customer_churn_web_app.py
@@ -177,8 +177,11 @@ def main():
         unsafe_allow_html=True
     )
 
-    # Header image with centered alignment
-    st.markdown('<img src="https://github.com/Exwhybaba/Customer_Churn/raw/main/Customer-Churn.png" alt="Predict Customer Churn" style="width:100%;">', unsafe_allow_html=True)
+    # Download and display the image
+    image_url = "https://github.com/Exwhybaba/Customer_Churn/raw/main/Customer-Churn.png"
+    image_content = requests.get(image_url).content
+    st.image(image_content, caption="Predict Customer Churn", use_column_width=True)
+
     # Sidebar layout with rounded corners
     st.sidebar.markdown(
         '<style>div.Widget.row-widget.stRadio div[role="radiogroup"] > label {border-radius: 10px;}</style>',
